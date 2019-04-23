e44e15a77f7b9813851d80001b471042227a513b
Ryan Sundberg <ryan.sundberg@gmail.com>
2019-04-23 16:57:55 -0700

    Release 0.2.0

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject arctype/steelyx "0.2.0-SNAPSHOT"
+(defproject arctype/steelyx "0.2.0"
   :dependencies 
   [[org.clojure/clojure "1.10.0"]
    [arctype/service "1.0.0"]
-   [arctype/service.curator "0.1.0-SNAPSHOT"
+   [arctype/service.curator "1.0.1"
     :exclusions [arctype/service]]
-   [arctype/service.quartzite "0.1.1-SNAPSHOT"
+   [arctype/service.quartzite "0.1.1"
     :exclusions [arctype/service]]
    [metosin/compojure-api "2.0.0-alpha29"]
-   [org.onyxplatform/onyx "0.14.4-arctype"]])
+   [org.onyxplatform/onyx "0.14.4"]])
