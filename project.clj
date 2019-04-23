81772ac5f26182b1587f5ed1001aa15c8fd8dc2e
Ryan Sundberg <ryan.sundberg@gmail.com>
2019-04-23 14:22:11 -0700

    Rename to Steelyx

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject arctype/service.onyx "0.2.0-SNAPSHOT"
+(defproject arctype/steelyx "0.2.0-SNAPSHOT"
   :dependencies 
   [[org.clojure/clojure "1.10.0"]
-   [arctype/service "0.1.0-SNAPSHOT"]
-   [arctype/service.curator "0.1.0-SNAPSHOT"]
-   [arctype/service.quartzite "0.1.1-SNAPSHOT"]
+   [arctype/service "1.0.0"]
+   [arctype/service.curator "0.1.0-SNAPSHOT"
+    :exclusions [arctype/service]]
+   [arctype/service.quartzite "0.1.1-SNAPSHOT"
+    :exclusions [arctype/service]]
    [metosin/compojure-api "2.0.0-alpha29"]
    [org.onyxplatform/onyx "0.14.4-arctype"]])
